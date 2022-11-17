67518ed08ccdb47d50854dc1583b44ba5826156f
maxlune <85277579+maxlune@users.noreply.github.com>
2022-11-17 12:26:28 +0100

    saving

--- main.js
+++ main.js
@@ -35,3 +35,41 @@ function LastName(props) {
 const helloWorld = <h1>Hello <FirstName text={firstName}/> <LastName text={lastName}/></h1>;
 
 ReactDOM.render(helloWorld, document.querySelector('#app'));
+
+function Clock(props) {
+    return (
+        <div>
+        <h1>Bonjour, monde !</h1>
+        <h2>Il est {props.date.toLocaleTimeString()}.</h2>
+        </div>
+    );
+    }
+
+    function tick() {
+    ReactDOM.render(
+        <Clock date={new Date()} />,
+        document.getElementById('root')
+    );
+}
+setInterval(tick, 1000);    
+
+// Class component :
+class Clock extends React.Component {
+    render() {
+       return(
+        <div>
+            <h1>Bonjour, monde !</h1>
+            <h2>Il est {this.props.date.toLocaleTimeString()}.</h2>
+        </div>
+        ) 
+    }
+}
+
+function Clock(props) {
+    return (
+        <div>
+            <h1>Bonjour, monde !</h1>
+            <h2>Il est {props.date.toLocaleTimeString()}.</h2>
+        </div>    
+    )
+}
