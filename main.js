a8b9282a86c9da47b7b3ca49d0a4770c697a44f9
maxlune <85277579+maxlune@users.noreply.github.com>
2022-11-17 09:21:59 +0100

    exo3

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-//const helloWorld = React.createElement('h1', {}, 'Hello world!');
 
 const helloWorld = <h1>Hello world!</h1>;
 
 ReactDOM.render(helloWorld, document.querySelector('#app'));
+
+const lastName = `label`;
+const firstName = `maximilien`;
+
+const element = 
+  <div>
+    <span>{firstName[0].toUpperCase() + firstName.slice(1)}</span> <span className="redText">{lastName}</span>
+  </div>;
+
+ReactDOM.render(element, document.querySelector('#app'));
