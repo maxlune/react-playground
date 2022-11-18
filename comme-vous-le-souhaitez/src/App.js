44f146f60f5da6f8c553eaba166041c70f0c030d
maxlune <85277579+maxlune@users.noreply.github.com>
2022-11-18 12:22:20 +0100

    saving

--- comme-vous-le-souhaitez/src/App.js
+++ comme-vous-le-souhaitez/src/App.js
@@ -1,9 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+import TodoInput from "./components/TodoInput";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [newTodo, setNewTodo] = useState({
+    id: 1,
+    title: '',
+    isCompleted : false,
+    isEditing: false
+})
+
+useEffect(() => {
+  console.log(newTodo);
+}, [newTodo])
+
+
+const handleTodoTitleChange = (e) => {
+  setNewTodo((prevState) => ({...prevState, title: e.target.value}))
+}
+
   return (
     <div className="App">
+      <TodoInput value={newTodo.title} handleTodoTitleChange={handleTodoTitleChange}/>
       {/* Le composant doit afficher :
         - L'input
         - le composant qui affiche les filtres
--- comme-vous-le-souhaitez/src/components/TodoInput.js
+++ comme-vous-le-souhaitez/src/components/TodoInput.js
@@ -1,5 +1,8 @@
 import React from "react";
 
-export default function TodoInput() {
-  return <div></div>;
+export default function TodoInput(props) {
+  return <input type="text" value={props.value} 
+    onChange={props.handleTodoTitleChange}
+    onKeyDown={null}
+    ></input>;
 }
